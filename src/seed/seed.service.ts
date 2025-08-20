43fee835b36a5fc161903bda57246415bc8a3cdd
Ismael Alvarez <ismaelalvarez514@gmail.com>
2025-08-19 23:41:42 -0600

    next commit

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-repsonse.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 
 @Injectable()
@@ -12,9 +12,9 @@ export class SeedService {
   
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http:AxiosAdapter,
   ){}
-  private readonly axios: AxiosInstance = axios;
 
 
 
@@ -23,7 +23,7 @@ export class SeedService {
     // first delete data
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await axios.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10");
+    const data = await this.http.get<PokeResponse>("https://pokeapi.co/api/v2/pokemon?limit=10");
 
     // ejemplo de una forma primer forma
     // const insertPromiseArray:Promise<Pokemon>[] = [];
